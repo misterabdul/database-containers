2e3e21b9464709458dc2a7712f3a15803d9a0455
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2022-07-27 18:28:38 +0700

    feat(mongo): change setup service to use the new mongosh
    Since mongo is deprecated, use the new mongosh. Didn't test the new
    shell script with mongosh command yet.

--- volumes/mongo-setup/mongo-setup.sh
+++ volumes/mongo-setup/mongo-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Starting MongoDB replica set..."
-mongo mongo:27017/$MONGO_INITDB_DATABASE -u$MONGO_INITDB_ROOT_USERNAME -p$MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase $MONGO_INITDB_DATABASE /external/scripts/mongo-setup.js
+mongosh mongo:27017/$MONGO_INITDB_DATABASE --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase $MONGO_INITDB_DATABASE --file /external/scripts/mongo-setup.js
 echo "Finished"
