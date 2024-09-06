5ffb7418c7dcabbcca734a61909bbd74ab15c069
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2024-09-06 11:14:40 +0700

    chore: remove unused directories & files

--- volumes/mongo-setup/mongo-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Starting MongoDB replica set..."
-mongosh mongo:27017/$MONGO_INITDB_DATABASE --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase $MONGO_INITDB_DATABASE --file /external/scripts/mongo-setup.js
-echo "Finished"
